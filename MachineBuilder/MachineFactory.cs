b1f5b8dcbd3cce2d5a409566dadbca7598ba33af
SamuilBachev <sbachev@yahoo.com>
2021-04-21 08:38:29 +0300

    new force engine line

--- MachineBuilder/MachineFactory.cs
+++ MachineBuilder/MachineFactory.cs
@@ -8,6 +8,7 @@ namespace MachineBuilder
             var myBike = myMachineBuilder.CreateMotorBike(2, 2, 200, 300f, 2000);
             myBike.ForceEngine(2001);
             myBike.Engine.ShowSerialNumber();
+            myBike.ForceEngine(1500);
         }
     }
 }
