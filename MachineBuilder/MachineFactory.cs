cecc4f58ac7a5cb928ecc518d52f410e485247ce
Nikolay Petrov <nlp@phys.uni-sofia.bg>
2025-03-26 12:25:01 +0200

    fixes on MachineConstructor and MachineFactory

--- MachineBuilder/MachineFactory.cs
+++ MachineBuilder/MachineFactory.cs
@@ -4,17 +4,20 @@ namespace MachineBuilder
     {
         static void Main(string[] args)
         {
-            MachineConstructor myMachineBuilder = new MachineConstructor("Steel Machines");
-            var myBike = myMachineBuilder.CreateMotorBike(2, 2, 200, 300f, 2000);
+            MachineConstructor mySteelMachinesBuilder = new MachineConstructor("Steel Machines");
+            var myBike = mySteelMachinesBuilder.CreateMotorBike(2, 2, 200, 300f, 2000);
             myBike.ForceEngine(2001);
+            myBike.RideBike();
             myBike.Engine.ShowSerialNumber();
             myBike.ForceEngine(1500);
-            
-            var denisBike = myMachineBuilder.CreateMotorBike(1, 2, 2000, 600f, 4000);
+
+            var denisBike = mySteelMachinesBuilder.CreateMotorBike(1, 2, 2000, 600f, 4000);
             System.Console.WriteLine("Denis Info");
             denisBike.ForceEngine(2500);
             denisBike.Engine.ShowSerialNumber();
             denisBike.ForceEngine(8000);
+
+            System.Console.WriteLine($"\nTotal machines built: {mySteelMachinesBuilder.MachineCounter}");
         }
     }
 }
